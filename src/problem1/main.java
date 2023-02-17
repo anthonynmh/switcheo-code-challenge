fa66b3ad420d079001a40c5db60908ce893ed5f2
anthonynmh <anthonyneomh@gmail.com>
2023-02-17 15:29:18 +0800

    Do Problem 1

--- src/problem1/main.java
+++ src/problem1/main.java
@@ -7,47 +7,32 @@ public class main {
     public main() {};
 
     public static void main(String args[]) {
-        //System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE);
 
-        int a = 0;
-        int b = 0;
-        int c = 0;
+        int n = 0;
 
         Scanner sc = new Scanner(System.in);
 
         try {
-            a = sc.nextInt();
-            b = sc.nextInt();
-            c = sc.nextInt();
+            n = sc.nextInt();
         } catch (Exception e) {
             System.out.println(e);
         }
 
         sc.close();
 
-        int sum1 = sum_to_n_a(a);
+        int sum1 = sum_to_n_a(n);
         System.out.println("sum1 = " + sum1);
 
-        int sum2 = sum_to_n_b(b);
+        int sum2 = sum_to_n_b(n);
         System.out.println("sum2 = " + sum2);
 
-        int sum3 = sum_to_n_c(c);
+        int sum3 = sum_to_n_c(n);
         System.out.println("sum3 = " + sum3);
     }
 
-    // Beauty of recursion
-    public static int sum_to_n_a(int n) {
-
-        // to prevent stack overflow
-        if (n > 30000) {
-            System.out.println("Sorry! Number is too big for sumToN1() :(");
-            return -1;
-        }
-        return n == 0 ? 0 : n + sum_to_n_a(n - 1);
-    }
-
     // Beauty of naivety
-    public static int sum_to_n_b(int n) {
+    public static int sum_to_n_a(int n) {
         int sum = 0;
         int count = n;
 
@@ -59,8 +44,29 @@ public class main {
         return sum;
     }
 
+    // Radix by 10
+    public static int sum_to_n_b(int n) {
+        
+        int numsOfSetsOf10 = n / 10;
+        int numOfRemainder = n % 10;
+        int sumOfOneToNine = 45;
+
+        int sum = 0;
+
+        for (int i = 0; i < numsOfSetsOf10; i++) {
+            sum += sumOfOneToNine;
+            sumOfOneToNine += 100;
+        }
+
+        for (int i = 0; i <= numOfRemainder; i++) {
+            sum += (numsOfSetsOf10 * 10) + i;
+        }
+
+        return sum;
+    }
+
     // Beauty of math
     public static int sum_to_n_c(int n) {
-        return (n * (1 + n) / 2);
+        return (int)(n * ((1 + n) / 2.0));
     }
 }
